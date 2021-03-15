20200d55e05bf249c18120d2c91a7ea6c444569c
Laurent Ellerbach <laurelle@microsoft.com>
2021-03-15 12:59:09 +0100

    Adding Unit Test from .NET Microframework (#25)
    ***NO_CI***

---
+++ Tests/NFUnitTestStringBuilder/EncodingTests.cs
@@ -0,0 +1,67 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.TestFramework;
+using System;
+using System.Text;
+
+namespace NFUnitTestStringBuilder
+{
+    [TestClass]
+    class EncodingTests
+    {
+        [TestMethod]
+        public void Utf8EncodingTests_Test1()
+        {
+            string str = "this is a normal string that will be used to convert to bytes then back to a string";
+            
+            byte[] data = new byte[128];
+            int len = str.Length;
+            int idx = 0;
+
+            Random rand = new Random();
+            
+            int cBytes = 0;
+
+            while (len > 0)
+            {
+                int size = (len <= 2) ? len : rand.Next(len / 2) + 1;
+                len -= size;
+
+                int cnt = UTF8Encoding.UTF8.GetBytes(str, idx, size, data, cBytes);
+
+                Assert.Equal(str.Substring(idx, size), new string(UTF8Encoding.UTF8.GetChars(data, cBytes, cnt)));
+
+                cBytes += cnt;
+                idx += size;
+            }
+            Assert.Equal(cBytes, str.Length);
+            string strAfter = new string(UTF8Encoding.UTF8.GetChars(data, 0, cBytes));
+            Assert.Equal(str, strAfter);
+        }
+
+        [TestMethod]
+        public void Utf8EncodingTests_Test2()
+        {
+            string str = "this is a normal string that will be used to convert to bytes then back to a string";
+            byte[] data = UTF8Encoding.UTF8.GetBytes(str);
+            Assert.Equal(data.Length, str.Length);
+            string strAfter = new string(UTF8Encoding.UTF8.GetChars(data));
+            Assert.Equal(str, strAfter);
+        }
+
+        [TestMethod]
+        public void Utf8EncodingTests_Test3()
+        {
+            // This tests involves a string with a special character
+            string str = "AB\u010DAB";
+            byte[] data = new byte[4];
+            int count = UTF8Encoding.UTF8.GetBytes(str, 1, 3, data, 0);
+            Assert.Equal(count, 4);
+            Assert.Equal(new string(UTF8Encoding.UTF8.GetChars(data)), "B\u010DA");
+        }
+    }
+}
---
+++ Tests/NFUnitTestStringBuilder/StringBuilderTests.cs
@@ -0,0 +1,256 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.TestFramework;
+using System;
+using System.Text;
+
+namespace NFUnitTestStringBuilder
+{
+    [TestClass]
+    public class StringBuilderTests
+    {
+        private static StringBuilder stringBuilder;
+
+        [Setup]
+        public void InitializeStringBuild()
+        {
+            stringBuilder = new StringBuilder();
+        }
+
+        [TestMethod]
+        public void Test_0_AppendTest_0()
+        {
+            stringBuilder.Append(true);
+            Assert.Equal(stringBuilder.ToString(), Boolean.TrueString);
+            stringBuilder.Append(false);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString));
+            stringBuilder.Append(byte.MinValue);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue));
+            stringBuilder.Append(new char[] { 'x', 'a' });
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa"));
+            stringBuilder.Append(double.Epsilon);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa", double.Epsilon.ToString()));
+            stringBuilder.Append(float.Epsilon);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa", double.Epsilon.ToString(), float.Epsilon.ToString()));
+            stringBuilder.Append(int.MaxValue);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa", double.Epsilon.ToString(), float.Epsilon.ToString(), int.MaxValue));
+            stringBuilder.Append(long.MaxValue);
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa", double.Epsilon.ToString(), float.Epsilon.ToString(), int.MaxValue, long.MaxValue));
+            stringBuilder.Append((object)"string");
+            Assert.Equal(stringBuilder.ToString(), string.Concat(Boolean.TrueString, Boolean.FalseString, byte.MinValue, char.MinValue, "xa", double.Epsilon.ToString(), float.Epsilon.ToString(), int.MaxValue, long.MaxValue, "string"));
+        }
+
+        [TestMethod]
+        public void Test_1_RemoveTest_0()
+        {
+            Assert.Equal(stringBuilder.Clear().ToString(), string.Empty);
+            string testString = "0123456789";
+            stringBuilder.Append(testString);
+            stringBuilder.Remove(0, 1);
+            Assert.Equal(stringBuilder.ToString(), "123456789");
+            stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            Assert.Equal(stringBuilder.ToString(), "12345678");
+        }
+
+        [TestMethod]
+        public void Test_2_InsertTest_0()
+        {
+            stringBuilder.Clear();
+            string testString = "0000";
+            stringBuilder.Append(testString);
+            stringBuilder.Insert(0, "x", 2);
+            Assert.Equal(stringBuilder.ToString(), "xx0000");
+            stringBuilder.Insert(stringBuilder.Length, "x", 2);
+            Assert.Equal(stringBuilder.ToString(), "xx0000xx");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_0()
+        {
+            stringBuilder.Clear();
+            string testString = "0000";
+            stringBuilder.Append(testString);
+            stringBuilder.Append("_");
+            stringBuilder.Append(testString);
+            stringBuilder.Replace(testString, "xx");
+            Assert.Equal(stringBuilder.ToString(), "xx_xx");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_1()
+        {
+            stringBuilder.Clear(); string testString = "BEGIN";
+            //stringBuilder.Append("BEGIN_MID_END");
+            stringBuilder.Append('B');
+            stringBuilder.Append('E');
+            stringBuilder.Append('G');
+            stringBuilder.Append('I');
+            stringBuilder.Append('N');
+            stringBuilder.Append('_');
+            stringBuilder.Append('M');
+            stringBuilder.Append('I');
+            stringBuilder.Append('D');
+            stringBuilder.Append('_');
+            stringBuilder.Append('E');
+            stringBuilder.Append('N');
+            stringBuilder.Append('D');
+            stringBuilder.Replace(testString, "xx");
+            Assert.Equal(stringBuilder.ToString(), "xx_MID_END");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_2()
+        {
+            string testString = "MID";
+            stringBuilder.Replace(testString, "xx");
+            Assert.Equal(stringBuilder.ToString(), "xx_xx_END");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_3()
+        {
+            string testString = "END";
+            stringBuilder.Replace(testString, "xx");
+            Assert.Equal(stringBuilder.ToString(), "xx_xx_xx");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_4()
+        {
+            string testString = "The quick br!wn d#g jumps #ver the lazy cat.";
+            stringBuilder = new System.Text.StringBuilder(testString);
+            stringBuilder.Replace('#', '!', 15, 29);        // Some '#' -> '!'
+            Assert.Equal(stringBuilder.ToString(), "The quick br!wn d!g jumps !ver the lazy cat.");
+            stringBuilder.Replace('!', 'o');                // All '!' -> 'o'
+            Assert.Equal(stringBuilder.ToString(), "The quick brown dog jumps over the lazy cat.");
+            stringBuilder.Replace("cat", "dog");            // All "cat" -> "dog"
+            Assert.Equal(stringBuilder.ToString(), "The quick brown dog jumps over the lazy dog.");
+            stringBuilder.Replace("dog", "fox", 15, 20);    // Some "dog" -> "fox"
+            Assert.Equal(stringBuilder.ToString(), "The quick brown fox jumps over the lazy dog.");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_5()
+        {
+            stringBuilder.Clear();
+            stringBuilder.Append("12345");
+            stringBuilder.Replace("45", "def");
+            Assert.Equal(stringBuilder.ToString(), "123def");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_6()
+        {
+            stringBuilder.Clear();
+
+            stringBuilder.Append("[{1234}]Test}]");
+            stringBuilder.Replace("}]", "}]example");
+            Assert.Equal(stringBuilder.ToString(), "[{1234}]exampleTest}]example");
+        }
+
+        [TestMethod]
+        public void Test_3_ReplaceTest_7()
+        {
+            var random = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                string sRaw, sFind, sReplace;
+                GenerateFuzzyParameters(out sRaw, out sFind, out sReplace, random);
+                stringBuilder.Clear();
+                stringBuilder.Append(sRaw);
+                stringBuilder.Replace(sFind, sReplace);
+                string sMFOutput = stringBuilder.ToString();
+                string sNativeOutput = NativeReplace(sRaw, sFind, sReplace);
+
+                Assert.Equal(sMFOutput, sNativeOutput);
+            }
+        }
+
+        void GenerateFuzzyParameters(out string sRaw, out string sFind, out string sReplace, Random random)
+        {
+            int cFind = random.Next(1, 4);
+            sFind = RandomString(random, 2, 6);
+            sReplace = RandomString(random, 4, 10);
+
+            sRaw = string.Empty;
+            for (int i = 0; i < cFind; i++)
+            {
+                if (i > 0 || random.Next() % 5 > 0)
+                {
+                    sRaw += RandomString(random, 2, 6);
+                }
+
+                sRaw += sFind;
+            }
+
+            if (random.Next() % 5 > 0)
+            {
+                sRaw += RandomString(random, 2, 6);
+            }
+        }
+
+        string RandomString(Random random, int iLenMin, int iLenMax)
+        {
+            string sChars = "abcdefghijklmnopqrstuvwxyz0123456789{}[]-=+()";
+            int length = random.Next(iLenMin, iLenMax);
+
+            string sOutput = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                sOutput += sChars[random.Next(0, sChars.Length - 1)];
+            }
+
+            return sOutput;
+        }
+
+        string NativeReplace(string sRaw, string sFind, string sReplace)
+        {
+            string sOutput = sRaw;
+            int i = 0;
+
+            while (i < sOutput.Length)
+            {
+                int p = sOutput.IndexOf(sFind, i);
+                if (p < 0)
+                {
+                    break;
+                }
+
+                sOutput = sOutput.Substring(0, p) + sReplace + sOutput.Substring(p + sFind.Length);
+                i = p + sReplace.Length;
+            }
+
+            return sOutput;
+        }
+
+        [TestMethod]
+        public void Test_4_CapacityTest_0()
+        {
+            stringBuilder.Length = 0;
+            stringBuilder.Capacity = 5;
+            Assert.Equal(stringBuilder.ToString(), string.Empty); string testString = "0000";
+            stringBuilder.Append(string.Empty);
+            stringBuilder.Append(testString);
+            stringBuilder.Append(string.Empty);
+            //should allocate here
+            stringBuilder.Append("_");
+            stringBuilder.Append("_");
+            //result is true if capacity is > 5
+            Assert.True(stringBuilder.Capacity > 5);
+        }
+
+    }
+
+    static class RandomExtension
+    {
+        static public int Next(this Random random, int iMin, int iMax)
+        {
+            return random.Next(iMax - iMin) + iMin;
+        }
+    }
+}
+
