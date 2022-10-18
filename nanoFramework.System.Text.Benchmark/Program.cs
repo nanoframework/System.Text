193eea07c92a829366ef256a8730ba5738daa9b3
Robb Schiefer <rschiefer@live.com>
2022-10-18 09:01:22 -0500

    added benchmark project

---
+++ nanoFramework.System.Text.Benchmark/Program.cs
@@ -0,0 +1,22 @@
+using nanoFramework.Benchmark;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace nanoFramework.System.Text.Benchmark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Debug.WriteLine("Hello from nanoFramework JSON benchmark!");
+            BenchmarkRunner.Run(typeof(IAssemblyHandler).Assembly);
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+    public interface IAssemblyHandler
+    {
+    }
+
+}
