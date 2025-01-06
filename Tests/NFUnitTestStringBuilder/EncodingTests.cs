648f33541c420b7450331d70f62139356bfc4218
José Simões <jose.simoes@eclo.solutions>
2025-01-06 14:45:36 +0000

    Migrate and add unit tests (#186)
    ***NO_CI***

--- Tests/NFUnitTestStringBuilder/EncodingTests.cs
+++ Tests/NFUnitTestStringBuilder/EncodingTests.cs
@@ -11,19 +11,19 @@ using System.Text;
 namespace NFUnitTestStringBuilder
 {
     [TestClass]
-    class EncodingTests
+    public class EncodingTests
     {
         [TestMethod]
         public void Utf8EncodingTests_Test1()
         {
             string str = "this is a normal string that will be used to convert to bytes then back to a string";
-            
+
             byte[] data = new byte[128];
             int len = str.Length;
             int idx = 0;
 
             Random rand = new Random();
-            
+
             int cBytes = 0;
 
             while (len > 0)
@@ -31,26 +31,26 @@ namespace NFUnitTestStringBuilder
                 int size = (len <= 2) ? len : rand.Next(len / 2) + 1;
                 len -= size;
 
-                int cnt = UTF8Encoding.UTF8.GetBytes(str, idx, size, data, cBytes);
+                int cnt = Encoding.UTF8.GetBytes(str, idx, size, data, cBytes);
 
-                Assert.Equal(str.Substring(idx, size), new string(UTF8Encoding.UTF8.GetChars(data, cBytes, cnt)));
+                Assert.AreEqual(str.Substring(idx, size), new string(Encoding.UTF8.GetChars(data, cBytes, cnt)));
 
                 cBytes += cnt;
                 idx += size;
             }
-            Assert.Equal(cBytes, str.Length);
-            string strAfter = new string(UTF8Encoding.UTF8.GetChars(data, 0, cBytes));
-            Assert.Equal(str, strAfter);
+            Assert.AreEqual(cBytes, str.Length);
+            string strAfter = new string(Encoding.UTF8.GetChars(data, 0, cBytes));
+            Assert.AreEqual(str, strAfter);
         }
 
         [TestMethod]
         public void Utf8EncodingTests_Test2()
         {
             string str = "this is a normal string that will be used to convert to bytes then back to a string";
-            byte[] data = UTF8Encoding.UTF8.GetBytes(str);
-            Assert.Equal(data.Length, str.Length);
-            string strAfter = new string(UTF8Encoding.UTF8.GetChars(data));
-            Assert.Equal(str, strAfter);
+            byte[] data = Encoding.UTF8.GetBytes(str);
+            Assert.AreEqual(data.Length, str.Length);
+            string strAfter = new string(Encoding.UTF8.GetChars(data));
+            Assert.AreEqual(str, strAfter);
         }
 
         [TestMethod]
@@ -59,9 +59,9 @@ namespace NFUnitTestStringBuilder
             // This tests involves a string with a special character
             string str = "AB\u010DAB";
             byte[] data = new byte[4];
-            int count = UTF8Encoding.UTF8.GetBytes(str, 1, 3, data, 0);
-            Assert.Equal(count, 4);
-            Assert.Equal(new string(UTF8Encoding.UTF8.GetChars(data)), "B\u010DA");
+            int count = Encoding.UTF8.GetBytes(str, 1, 3, data, 0);
+            Assert.AreEqual(4, count);
+            Assert.AreEqual("B\u010DA", new string(Encoding.UTF8.GetChars(data)));
         }
     }
 }
